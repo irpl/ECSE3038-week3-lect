47e2b2e7140d38b200fc6e31f407f467c0d79033
Phillip Logan <phillip.logan@digicelgroup.com>
2022-02-07 12:27:20 -0500

    added all the other stuff

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask_pymongo import PyMongo
 from marshmallow import Schema, fields
 from bson.json_util import dumps
 from json import loads
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://week3:1yDxm2EdXKkIwby4@cluster0.xtk0f.mongodb.net/fruit_basket?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = os.getenv("MONGO_CONNECTION_STRING")
 mongo = PyMongo(app)
 
 
@@ -13,13 +17,13 @@ class FruitSchema(Schema):
   name = fields.String(required=True)
   sugar_content = fields.Integer(required=True)
   colour = fields.String(required=True)
-  country = fields.String(required=True)
+  calories = fields.Integer(required=True)
 
 
 @app.route("/fruit", methods=["POST"])
-def add_new_fruit(id):
+def add_new_fruit():
   request_dict = request.json
-  new_fruit = FruitSchema.load(request_dict)
+  new_fruit = FruitSchema().load(request_dict)
 
   fruit_document = mongo.db.fruits.insert_one(new_fruit)
   fruit_id = fruit_document.inserted_id
@@ -30,3 +34,39 @@ def add_new_fruit(id):
 
   return jsonify(fruit_json)
 
+
+@app.route("/fruit", methods=["GET"])
+def get_fruits():
+  fruits = mongo.db.fruits.find()
+  fruits_list = loads(dumps(fruits))
+
+  return jsonify(fruits_list)
+
+
+#! FIX THIS CUZ IT BREAKS
+@app.route("/fruit/<ObjectId:id>", methods=["PATCH"])
+def update_fruit(id):
+  mongo.db.fruits.update_one({"_id": id}, {"$set": request.data })
+
+  fruit = mongo.db.fruits.find_one(id)
+
+  fruit_json = loads(dumps(fruit))
+  return fruit_json
+
+
+@app.route("/fruit/<ObjectId:id>", methods=["DELETE"])
+def delete_fruit(id):
+  result = mongo.db.fruits.delete_one({"_id": id})
+
+  if result.deleted_count == 1:
+    return {
+      "success": True
+    }
+  else:
+    return {
+      "success": False
+    }, 400
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host="0.0.0.0", port="3001")
